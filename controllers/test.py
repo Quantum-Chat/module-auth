57b680203819fb8535c8a72c67be05674bee1753
duckuments <duckuments@gmail.com>
2025-12-17 18:55:28 -0800

    test: 💍 test insert row
    i test insert mode and it's woooork

--- controllers/test.py
+++ controllers/test.py
@@ -4,10 +4,13 @@ sys.path.append("/home/Hirmaan/projects/module-auth/")
 
 
 from models.repository import Repository
+from models.models import Client
 
 
 def main():
+    row = Client(token="this a test for insert")
     repo = Repository()
+    repo.insert(row)
 
 
 if __name__ == "__main__":
